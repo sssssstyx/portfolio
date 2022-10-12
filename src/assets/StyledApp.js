c8f5159b6b18d0e674e79817c248b17ee021be9f
unamedshy <2977197462@qq.com>
2022-10-12 19:57:21 +0300

    fix some bugs

--- src/assets/StyledApp.js
+++ src/assets/StyledApp.js
@@ -26,26 +26,6 @@ const StyledParallax = tw.div`
     flex
     flex-col
     md:flex-row
-    snap-mandatory
-    snap-y
-    md:snap-x
-    md:snap-proximity
 `
-/* // This is the earlier version of Styled Parallax
-const StyledParallax = tw(NoScrollBarStyles)`
-    absolute
-    md:relative
-    w-full
-    flex
-    flex-col
-    md:flex-row
-    
-    overflow-x-hidden
-    md:overflow-y-hidden
-    snap-proximity
-    snap-y
-    md:snap-x
-    scroll-smooth
-`*/
 
 export default StyledApp
